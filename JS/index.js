09a18ed90bd9b61633b03aca42e8f6a0c0dc1f08
Aya Ibrahim <Aya.Ibrahim@itworx.com>
2022-07-20 22:04:02 +0200

    first commit

---
+++ JS/index.js
@@ -0,0 +1,135 @@
+$cards = [
+  {
+    title: "EGYPT TANGBIA",
+    img: "http://www.cairowestmag.com/wp-content/uploads/2014/04/Nubian.jpg",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "OLLING PHISOTIP",
+    img: "https://th.bing.com/th/id/OIP.PXSEfTBXtn3RisercyTQaQHaFS?w=243&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "CORTIA DYNA",
+    img: "https://th.bing.com/th/id/OIP.Voo-74QP0yVe0GirnOwqRwHaFj?w=207&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "SYSNE DUBAI",
+    img: "https://th.bing.com/th/id/OIP.1tQI__InMYemlFLsYPsp3gHaE7?w=270&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "EGYPT TANGBIA",
+    img: "https://i1.wp.com/helloraya.com/wp-content/uploads/2016/03/Mohd-Ali-Mosque-Cairo.jpg?resize=940%2C705&ssl=1",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "OLLING PHISOTIP",
+    img: "https://th.bing.com/th/id/OIP.LrDMm-SMWle7UErsDPUfHgHaE8?w=243&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "CORTIA DYNA",
+    img: "https://th.bing.com/th/id/OIP.veO1JC0kzpmu6UE9jT0rAwHaC9?w=295&h=140&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "SYSNE DUBAI",
+    img: "https://th.bing.com/th/id/OIP.EozMXnserU5Ahfs9BRNC4AAAAA?w=267&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "EGYPT TANGBIA",
+    img: "https://th.bing.com/th/id/OIP.-Zl45jaI9cr1p6G8sWaGIwHaFj?w=228&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "OLLING PHISOTIP",
+    img: "https://th.bing.com/th/id/OIP.qt9w8hp_xQS7ciKx9pAi1gHaFL?w=238&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "CORTIA DYNA",
+    img: "https://th.bing.com/th/id/OIP.9hEn4IJHXgFMrMO4crH5DgHaFj?w=239&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+  {
+    title: "SYSNE DUBAI",
+    img: "https://th.bing.com/th/id/OIP.nH4L5BimEFPp4nPmHtER0QHaFi?w=204&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    prg: "Denatibus et magis dis parturient montes, nascetur ridiculus mus",
+    prgFull:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Nam nibh. Nunc varis facilisis eros, Sed erat. In inVelit quis arcu ornare laoreet. Curabitur.",
+    date: "Aug 5, 2013",
+    comments: "22 Comments",
+  },
+];
+
+$.each($cards, function (idx, value) {
+  $intialCardId = "card-" + (idx + 1);
+  $("#img-" + $intialCardId).attr("src", value.img);
+  $("#title-" + $intialCardId).text(value.title);
+  $("#prag-" + $intialCardId).text(value.prg);
+  $("#date-" + $intialCardId).text(value.date);
+  $("#com-" + $intialCardId).text(value.comments);
+});
+
+$(document).ready(function () {
+  $(".card").click(function () {
+    $cardId = $(this).attr("id");
+    $cardNum = $cardId.replace("card-", "") - 1;
+    $cardModalId = "#modal-img-" + $cardId;
+    $cardImgId = "#img-" + $cardId;
+    $cardTitleId = "#title-" + $cardId;
+    $cardImg = $($cardImgId).attr("src");
+    $cardTitle = $($cardTitleId).text();
+
+    $("#modal-img-" + $cardId).attr("src", $cardImg);
+    $("#modal-title-" + $cardId).text($cardTitle);
+    $(".modal-body-" + $cardId).text($cards[$cardNum].prgFull);
+  });
+});
